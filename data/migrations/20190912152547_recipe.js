ac8a03e4452f7f5bdb10a8fe3dcce65e477ce2b4
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-12 15:42:15 -0600

    Created migration table schema

--- data/migrations/20190912152547_recipe.js
+++ data/migrations/20190912152547_recipe.js
@@ -5,25 +5,45 @@ exports.up = function (knex) {
             //PK
             tbl.increments();
             //name
-            tbl.string()
+            tbl
+                .string('recipe_name', 255)
+                .notNullable()
         })
 
         .createTable('ingredients', tbl => {
             //PK
             tbl.increments();
             //name
-            tbl.string()
+            tbl.string('ingredients_name', 255)
         })
 
-        .createTable('recipe', tbl => {
+        .createTable('recipe_ingredients', tbl => {
             //FK
+            tbl
+                .integer('recipe_id')
+                .unsigned()
+                .references('id')
+                .inTable('recipe')
+                .onDelete('CASCADE')// if the PK record is deleted
+                .onUpdate('CASCADE')// if the PK record is updates
 
             //FK
-            
+            tbl
+                .integer('ingredients_id')
+                .unsigned()
+                .references('id')
+                .inTable('ingredients')
+                .onDelete('CASCADE')// if the PK record is deleted
+                .onUpdate('CASCADE')// if the PK record is updates
+
             //Composite Key: composes fo the FK's from above. Will be unique
+            tbl.primary(['recipe_id', 'ingredients_id'])
         })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema
+        .dropTableIfExists('ingredients_id')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipe')
 };
